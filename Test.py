6c3928d7a534b5b41ce0a6bfb79e4a414bc2a8b5
Mrsans8933 <159053603+Mrsans8933@users.noreply.github.com>
2026-04-20 17:05:41 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,17 @@
+import pyperclip
+import keyboard
+import winsound
+all_letters = {'q': 'й', 'w': 'ц', 'e': 'у', 'r': 'к', 't': 'е', 'y': 'н', 'u': 'г',
+    'i': 'ш', 'o': 'щ', 'p': 'з', '[': 'х', ']': 'ъ', 'a': 'ф', 's': 'ы',
+    'd': 'в', 'f': 'а', 'g': 'п', 'h': 'р', 'j': 'о', 'k': 'л', 'l': 'д',
+    ';': 'ж', "'": 'э', 'z': 'я', 'x': 'ч', 'c': 'с', 'v': 'м', 'b': 'и',
+    'n': 'т', 'm': 'ь', ',': 'б', '.': 'ю', '/': '.'}
+new_copy = []
+while True:
+    keyboard.wait("Alt+C")
+    winsound.Beep(1000,100)
+    now = pyperclip.paste()
+    for letter in now:
+        adder = all_letters.get(letter)
+        new_copy.append(adder)
+    print(new_copy)
