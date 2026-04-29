1b9d2f35fbfc33649945839bc35de2666baf5bea
Mrsans8933 <159053603+Mrsans8933@users.noreply.github.com>
2026-04-29 12:26:32 +0300

    Update Password_generator.py
    Добавил обработку ошибок.

--- Password_generator/Password_generator.py
+++ Password_generator/Password_generator.py
@@ -1,22 +1,34 @@
 import random
+
 numbers = [1,2,3,4,5,6,7,8,9,0]
-bukvi = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
 choose = 0
 password = []
 print("Привет, это генератор паролей! Введи длину желанного пароля")
-long = int(input())
+
+try:
+    long = int(input())
+except ValueError:
+    print("Ошибка вы ввели не число")
+    long = int(input("Введи число: "))
 print("Выбери вид пароля: 1 - только цифры; 2 - только буквы; 3 - все вместе")
-choose = int(input())
+
+try:
+    choose = int(input())
+except ValueError:
+    print("Ошибка вы ввели не число")
+    choose = int(input("Введите число: "))
+    
 if choose == 1:
     for i in range(long):
         password.append(random.choice(numbers))
 if choose == 2:
     for i in range(long):
-        password.append(random.choice(bukvi))
+        password.append(random.choice(letters))
 if choose == 3:
-    all = "0123456789" + bukvi
+    all_symbols = "0123456789" + letters
     for i in range(long):
-        password.append(random.choice(all))
+        password.append(random.choice(all_symbols))
 
 
 
