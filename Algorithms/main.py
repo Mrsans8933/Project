31c5b5099f95c04a9bbdde4fd664a0bc31b7c3cb
Mrsans8933 <159053603+Mrsans8933@users.noreply.github.com>
2026-05-17 13:39:26 +0300

    Зделал пример алгоритма  О(log n)

---
+++ Algorithms/main.py
@@ -0,0 +1,22 @@
+# Пример бинарного поиска O(log n)
+minim = 1
+maxim = int(input("Введи максимальное число"))
+print(f"Загадай число в интервале от 1 до {maxim}")
+attems = 0
+
+while minim <= maxim:
+    attems += 1
+    mid = (minim + maxim) // 2
+    pl = input(f"Твое число больше {mid}?")
+    if pl == "+":
+        minim = mid+1
+    elif pl == "-":
+        maxim = mid-1
+    elif pl == "=":
+        print(f"Попыток {attems}")
+        exit()
+    else:
+        print("Ошибка принамаются только + - =")
+
+
+print("Ошибка возможно вы ошиблись")
